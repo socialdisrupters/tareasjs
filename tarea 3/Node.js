28f9a4f827d3b7a8cf0f5e22c00914b01bb7abce
Mario Antonio <mario.pineda@socialdisrupters.org>
2020-07-11 17:51:04 -0500

    Cambio para el visitante

---
+++ tarea 3/Node.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const servidor = express ();
+
+let num = 1;
+
+servidor.get('/', function(req, res){
+    res.send('Eres el visitante' + num++);
+});
+
+servidor.listen(3000,function(){
+    console.log('Estoy escuchando en el puerto 3000');
+});
